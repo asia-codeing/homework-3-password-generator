b214aab96d322ba6bd56702879971444b2ab0b61
Asia Alnahi <asia.alius@gmail.com>
2021-02-20 20:37:26 -0800

    Update the README file and change the if statment to while loop to make the code work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,24 @@
 var generateBtn = document.querySelector("#generate"); 
 
 function criteria() {
-  var length = prompt("How many characters would you like your password to be? Your password lenght should be 8-128 characters.");
-
+  var length = prompt("How many characters would you like your password to be?");
   console.log(length, "length" );
 
-  // if your length is less than 8 or greater than 128, then runs this if statement
-  if (length < 8 || length > 128) {
-    // run the alert if length is not acceptable
-    alert("Your password lenght should be 8-128 characters.");
-    // run this whole function again  
-    criteria();
-    } 
+  // your length is less than 8 or greater than 128, then runs this statement
+  while (length < 8 || length > 128) {
+
+    // Run the prompt again if length is not acceptable
+    length = prompt("Length must be 8-128 characters. How many characters would you like your password to be?");
     
-    // else statement
-    
+  }
     var lowercase = confirm("Would you like to use lowercase letters?");
     var uppercase = confirm("Would you like to use uppercase letters?");
     var numbers = confirm("would you like to use numbers?");
     var symbols = confirm("would you like to use special characters?");
 
 
-  if (!(lowercase || uppercase || numbers || symbols)) {
+  //Run this loop if you didn't select any criteria 
+  while (!(lowercase || uppercase || numbers || symbols)) {
     window.alert("You should select at least one character type! ");
     var lowercase = confirm("Would you like to use lowercase letters?");
     var uppercase = confirm("Would you like to use uppercase letters?");
@@ -30,12 +27,14 @@ function criteria() {
     var symbols = confirm("would you like to use special characters?");
   }
 
+  // If the length equal or greater than 8 and equal or less than 128
+  if (length >= 8 && length <= 128){
+  //Run the generate password function 
   generatePassword(lowercase, uppercase, numbers, symbols, length);
-  
+  }
 };
 
 // Write password to the #password input
-
 const randomFunc = {
   lower: getRandomLowerCase,
   upper: getRandomUpperCase,
@@ -46,12 +45,13 @@ const randomFunc = {
 function generatePassword(lower, upper, number, symbol, length) {
   let generatedPassword = '';
   const typesCount = lower + upper + number + symbol;
+  // Array for generator functions
   const typesArr = [
-    { lower },
-    { upper },
-    { number },
-    { symbol }
-  ].filter(item => Object.values(item)[0]);
+    { lower 
+    },{ upper
+    },{ number 
+    },{ symbol 
+    }].filter(item => Object.values(item)[0]);//This function filter the selected criterias 
   console.log(lower, upper, number, symbol, length)
   
   // create a loop
@@ -82,8 +82,7 @@ function getRandomSymbol(){
   var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
   return symbol[Math.floor(Math.random() * symbol.length)];
 }
-
-
+//This function to put the password in the textarea
 function writePassword(password) {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
